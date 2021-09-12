67bb55dbafb8b23cc9791e4bc41aa5c736643812
Owl-In-Bowl <nicolas.apollon@wanadoo.fr>
2021-09-12 16:31:52 +0200

    ajout de post dans git

---
+++ src/app/post/post.component.ts
@@ -0,0 +1,41 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.scss']
+})
+export class PostComponent implements OnInit {
+  @Input() postAutor: string = 'Paul';
+  @Input() postTitle: string ='du nouveau vin !';
+  @Input() postContent: string = 'le nouveau nez est arrivé ce samedi en bourgogne';
+  nbLike = 0;
+  nbDislike=0;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  getPostContent () {
+    return this.postContent;
+  }
+
+  addLike(){
+    this.nbLike +=1 ;
+  }
+
+  addDislike(){
+    this.nbDislike+=1;
+  }
+
+  getTendance(){
+    if (this.nbDislike<=this.nbLike){
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+}
